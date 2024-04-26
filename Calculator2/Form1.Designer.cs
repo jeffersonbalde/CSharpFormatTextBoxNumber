b25dbc392cd5d59e3d62b53b6040785bb02ab3ef
jeffersonbalde <jeffersonbalde8@gmail.com>
2024-04-26 13:35:15 +0800

    Add project files.

---
+++ Calculator2/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Calculator2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.buttonEquals = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 34.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox1.Location = new System.Drawing.Point(24, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(458, 83);
+            this.textBox1.TabIndex = 84;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 34.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox2.Location = new System.Drawing.Point(24, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(458, 83);
+            this.textBox2.TabIndex = 85;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI Semibold", 34.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox3.Location = new System.Drawing.Point(25, 234);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(458, 83);
+            this.textBox3.TabIndex = 86;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // buttonEquals
+            // 
+            this.buttonEquals.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(182)))), ((int)(((byte)(114)))));
+            this.buttonEquals.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonEquals.FlatAppearance.BorderSize = 0;
+            this.buttonEquals.Font = new System.Drawing.Font("Segoe UI Semibold", 28F, System.Drawing.FontStyle.Bold);
+            this.buttonEquals.ForeColor = System.Drawing.Color.White;
+            this.buttonEquals.Location = new System.Drawing.Point(24, 332);
+            this.buttonEquals.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonEquals.Name = "buttonEquals";
+            this.buttonEquals.Size = new System.Drawing.Size(458, 80);
+            this.buttonEquals.TabIndex = 87;
+            this.buttonEquals.Text = "=";
+            this.buttonEquals.UseVisualStyleBackColor = false;
+            this.buttonEquals.Click += new System.EventHandler(this.buttonEquals_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Consolas", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 451);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(457, 61);
+            this.label1.TabIndex = 88;
+            this.label1.Text = "0.00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(509, 551);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonEquals);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        public System.Windows.Forms.Button buttonEquals;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Calculator2/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.TextChanged -= textBox1_TextChanged;
+
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    double number = double.Parse(textBox1.Text.Replace(",", ""));
+                    textBox1.Text = number.ToString("#,##0"); 
+                    textBox1.SelectionStart = textBox1.Text.Length;
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = "";
+            }
+
+            textBox1.TextChanged += textBox1_TextChanged;
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.TextChanged -= textBox2_TextChanged; 
+
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(textBox2.Text))
+                {
+                    double number = double.Parse(textBox2.Text.Replace(",", "")); 
+                    textBox2.Text = number.ToString("#,##0"); 
+                    textBox2.SelectionStart = textBox2.Text.Length; 
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox2.Text = "";
+            }
+
+            textBox2.TextChanged += textBox2_TextChanged;
+        }
+
+        private void buttonEquals_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+            {
+                MessageBox.Show("Fill up all fields");
+                return;
+            }
+
+            if(textBox3.Text == "+" || textBox3.Text == "-" || textBox3.Text == "*" || textBox3.Text == "/")
+            {
+                if (textBox3.Text == "+")
+                {
+                    double result = double.Parse(textBox1.Text) + double.Parse(textBox2.Text);
+                    label1.Text = result.ToString("#,##0");
+                }
+
+                if (textBox3.Text == "-")
+                {
+                    double result = double.Parse(textBox1.Text) - double.Parse(textBox2.Text);
+                    label1.Text = result.ToString("#,##0");
+                }
+
+                if (textBox3.Text == "*")
+                {
+                    double result = double.Parse(textBox1.Text) * double.Parse(textBox2.Text);
+                    label1.Text = result.ToString("#,##0");
+                }
+
+                if (textBox3.Text == "/")
+                {
+                    double result = double.Parse(textBox1.Text) / double.Parse(textBox2.Text);
+                    label1.Text = result.ToString("#,##0");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Operator");
+                return;
+            }
+        }
+    }
+}
